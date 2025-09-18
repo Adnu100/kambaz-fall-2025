3c35424778dba7b6d512afd8310c1d9b334db357
Adnesh Dhamangaonkar <dhamangaonadnesh@gmail.com>
2025-09-18 18:13:46 -0400

    completed lab2 until Kambaz changes part

--- src/app/Labs/TOC.tsx
+++ src/app/Labs/TOC.tsx
@@ -1,33 +1,39 @@
+"use client";
+
+import Nav from "react-bootstrap/Nav";
 import Link from "next/link";
 
 export default function TOC() {
   return (
-    <ul>
-      <li>
-        <Link href="/Labs" id="wd-lab1-link">
+    <Nav variant="pills">
+      <Nav.Item>
+        <Nav.Link href="/Labs" as={Link}>
           Home
-        </Link>
-      </li>
-      <li>
-        <Link href="/Labs/Lab1" id="wd-lab1-link">
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/Labs/Lab1" as={Link}>
           Lab 1
-        </Link>
-      </li>
-      <li>
-        <Link href="/Labs/Lab2" id="wd-lab2-link">
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/Labs/Lab2" as={Link}>
           Lab 2
-        </Link>
-      </li>
-      <li>
-        <Link href="/Labs/Lab3" id="wd-lab3-link">
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/Labs/Lab3" as={Link}>
           Lab 3
-        </Link>
-      </li>
-      <li>
-        <Link href="/" id="wd-lab3-link">
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/" as={Link}>
           Kambaz
-        </Link>
-      </li>
-    </ul>
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="https://github.com/Adnu100">My GitHub</Nav.Link>
+      </Nav.Item>
+    </Nav>
   );
 }
