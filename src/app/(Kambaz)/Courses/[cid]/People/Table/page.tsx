70aeb4795a79ed8d7e3f9ef65ecb658d5bfbbad6
Adnesh Dhamangaonkar <dhamangaonadnesh@gmail.com>
2025-09-22 15:54:34 -0400

    completed till on your own part

---
+++ src/app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -0,0 +1,95 @@
+import { Table } from "react-bootstrap";
+import { FaUserCircle } from "react-icons/fa";
+
+export default function PeopleTable() {
+  return (
+    <div id="wd-people-table">
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Login ID</th>
+            <th>Section</th>
+            <th>Role</th>
+            <th>Last Activity</th>
+            <th>Total Activity</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Tony</span>
+              <span className="wd-last-name">Stark</span>
+            </td>
+            <td className="wd-login-id">001234561S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-01</td>
+            <td className="wd-total-activity">10:21:32</td>
+          </tr>
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Bruce</span>
+              <span className="wd-last-name">Wayne</span>
+            </td>
+            <td className="wd-login-id">001234562B</td>
+            <td className="wd-section">S102</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-02</td>
+            <td className="wd-total-activity">12:15:48</td>
+          </tr>
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Natasha</span>
+              <span className="wd-last-name">Romanoff</span>
+            </td>
+            <td className="wd-login-id">001234563N</td>
+            <td className="wd-section">S103</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-03</td>
+            <td className="wd-total-activity">08:42:11</td>
+          </tr>
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Clark</span>
+              <span className="wd-last-name">Kent</span>
+            </td>
+            <td className="wd-login-id">001234564C</td>
+            <td className="wd-section">S104</td>
+            <td className="wd-role">TEACHER</td>
+            <td className="wd-last-activity">2020-10-04</td>
+            <td className="wd-total-activity">15:33:59</td>
+          </tr>
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Diana</span>
+              <span className="wd-last-name">Prince</span>
+            </td>
+            <td className="wd-login-id">001234565D</td>
+            <td className="wd-section">S105</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-05</td>
+            <td className="wd-total-activity">09:58:24</td>
+          </tr>
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Peter</span>
+              <span className="wd-last-name">Parker</span>
+            </td>
+            <td className="wd-login-id">001234566P</td>
+            <td className="wd-section">S106</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-06</td>
+            <td className="wd-total-activity">11:07:13</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+}
--- src/app/(Kambaz)/Dashboard/page.tsx
+++ src/app/(Kambaz)/Dashboard/page.tsx
@@ -1,5 +1,14 @@
 import Link from "next/link";
-import Image from "next/image";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardImg,
+  CardText,
+  CardTitle,
+  Col,
+  Row,
+} from "react-bootstrap";
 
 export default function Dashboard() {
   return (
@@ -8,53 +17,197 @@ export default function Dashboard() {
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
       <div id="wd-dashboard-courses">
         <div className="wd-dashboard-course">
-          <Link href="/Courses/1234" className="wd-dashboard-course-link">
-            <Image
-              alt="course image"
-              src="/images/reactjs.png"
-              width={200}
-              height={150}
-            />
-            <div>
-              <h5> CS1234 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/2235" className="wd-dashboard-course-link">
-            <Image
-              alt="course image"
-              src="/images/nodejs.png"
-              width={200}
-              height={150}
-            />
-            <div>
-              <h5> CS2235 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Backend software developer
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/2553" className="wd-dashboard-course-link">
-            <Image
-              alt="course image"
-              src="/images/dbms.jpeg"
-              width={200}
-              height={150}
-            />
-            <div>
-              <h5> CS2553 Database Management Systems </h5>
-              <p className="wd-dashboard-course-title">Database engineer</p>
-              <button> Go </button>
-            </div>
-          </Link>
+          <Row xs={1} md={5} className="g-4">
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+                <Link
+                  href="/Courses/1234/Home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <CardImg
+                    variant="top"
+                    src="/images/reactjs.png"
+                    width="100%"
+                    height={160}
+                  />
+                  <CardBody>
+                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                      CS1234 React JS
+                    </CardTitle>
+                    <CardText
+                      className="wd-dashboard-course-description overflow-hidden"
+                      style={{ height: "100px" }}
+                    >
+                      Full Stack software developer
+                    </CardText>
+                    <Button variant="primary">Go</Button>
+                  </CardBody>
+                </Link>
+              </Card>
+            </Col>
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+                <Link
+                  href="/Courses/2235/Home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <CardImg
+                    variant="top"
+                    src="/images/nodejs.png"
+                    width="100%"
+                    height={160}
+                  />
+                  <CardBody>
+                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                      CS2235 Node JS
+                    </CardTitle>
+                    <CardText
+                      className="wd-dashboard-course-description overflow-hidden"
+                      style={{ height: "100px" }}
+                    >
+                      Backend software developer
+                    </CardText>
+                    <Button variant="primary">Go</Button>
+                  </CardBody>
+                </Link>
+              </Card>
+            </Col>
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+                <Link
+                  href="/Courses/2553/Home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <CardImg
+                    variant="top"
+                    src="/images/dbms.jpeg"
+                    width="100%"
+                    height={160}
+                  />
+                  <CardBody>
+                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                      CS2553 Database Management Systems
+                    </CardTitle>
+                    <CardText
+                      className="wd-dashboard-course-description overflow-hidden"
+                      style={{ height: "100px" }}
+                    >
+                      Database Engineer
+                    </CardText>
+                    <Button variant="primary">Go</Button>
+                  </CardBody>
+                </Link>
+              </Card>
+            </Col>
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+                <Link
+                  href="/Courses/2540/Home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <CardImg
+                    variant="top"
+                    src="/images/algorithm.avif"
+                    width="100%"
+                    height={160}
+                  />
+                  <CardBody>
+                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                      CS2540 Algorithms
+                    </CardTitle>
+                    <CardText
+                      className="wd-dashboard-course-description overflow-hidden"
+                      style={{ height: "100px" }}
+                    >
+                      Computer Science Mandatory Course
+                    </CardText>
+                    <Button variant="primary">Go</Button>
+                  </CardBody>
+                </Link>
+              </Card>
+            </Col>
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+                <Link
+                  href="/Courses/2192/Home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <CardImg
+                    variant="top"
+                    src="/images/datascience.jpg"
+                    width="100%"
+                    height={160}
+                  />
+                  <CardBody>
+                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                      CS2192 Data Science
+                    </CardTitle>
+                    <CardText
+                      className="wd-dashboard-course-description overflow-hidden"
+                      style={{ height: "100px" }}
+                    >
+                      Data Scientist
+                    </CardText>
+                    <Button variant="primary">Go</Button>
+                  </CardBody>
+                </Link>
+              </Card>
+            </Col>
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+                <Link
+                  href="/Courses/3132/Home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <CardImg
+                    variant="top"
+                    src="/images/angularjs.jpg"
+                    width="100%"
+                    height={160}
+                  />
+                  <CardBody>
+                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                      CS3132 Angular JS
+                    </CardTitle>
+                    <CardText
+                      className="wd-dashboard-course-description overflow-hidden"
+                      style={{ height: "100px" }}
+                    >
+                      Frontend Software Developer
+                    </CardText>
+                    <Button variant="primary">Go</Button>
+                  </CardBody>
+                </Link>
+              </Card>
+            </Col>
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+                <Link
+                  href="/Courses/2491/Home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <CardImg
+                    variant="top"
+                    src="/images/python.png"
+                    width="100%"
+                    height={160}
+                  />
+                  <CardBody>
+                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                      CS2491 Python Programming
+                    </CardTitle>
+                    <CardText
+                      className="wd-dashboard-course-description overflow-hidden"
+                      style={{ height: "100px" }}
+                    >
+                      Machine Learning Scientist
+                    </CardText>
+                    <Button variant="primary">Go</Button>
+                  </CardBody>
+                </Link>
+              </Card>
+            </Col>
+          </Row>
         </div>
       </div>
     </div>
